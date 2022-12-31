b399d0e3f2989d7e4bbc694430e3829762817190
Michael Wu <mw10013@gmail.com>
2022-12-31 10:05:46 -0500

    use storefrontApiCustomScalars

--- graphql-codegen.config.ts
+++ graphql-codegen.config.ts
@@ -1,6 +1,5 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
-// Due to issues with TurboRepo and dev dependencies, we can't use this in the monorepo. But if we could, then it would look like this import, and then used below in "scalars"
-// import {storefrontApiCustomScalars} from '@shopify/hydrogen-react';
+import { storefrontApiCustomScalars } from "@shopify/hydrogen-react";
 
 const config: CodegenConfig = {
   schema: "./node_modules/@shopify/hydrogen-react/storefront.schema.json",
@@ -16,16 +15,7 @@ const config: CodegenConfig = {
         enumsAsTypes: true,
         useTypeImports: true,
         useImplementingTypes: true,
-        // scalars: storefrontApiCustomScalars,
-        scalars: {
-          // Because of the limitations outlined above, these need to be kept in sync with the original definitions in the @shopify/hydrogen-react repo!
-          DateTime: "string",
-          Decimal: "string",
-          HTML: "string",
-          URL: "string",
-          Color: "string",
-          UnsignedInt64: "string",
-        },
+        scalars: storefrontApiCustomScalars,
       },
     },
   },
