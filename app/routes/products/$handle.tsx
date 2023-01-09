ee859aa7d9a943d6dca9b6aad0b3d7d298234ee1
Michael Wu <mw10013@gmail.com>
2023-01-09 17:49:55 -0500

    product detail

--- app/routes/products/$handle.tsx
+++ app/routes/products/$handle.tsx
@@ -9,6 +9,8 @@ import { NotFound } from "~/components/global/not-found";
 import { graphql } from "~/lib/gql/gql";
 import { getExcerpt, shopClient } from "~/lib/utils";
 import { Text } from "~/components/elements/text";
+import { ProductProvider } from "@shopify/hydrogen-react";
+import { ProductDetail } from "~/components/product/product-detail";
 
 const query = graphql(`
   query Product($handle: String!) {
@@ -105,10 +107,53 @@ export default function Product() {
   } = product.variants.nodes[0];
 
   return (
-    <div>
-      <pre>{JSON.stringify(product, null, 2)}</pre>
-      <pre>{JSON.stringify(shop, null, 2)}</pre>
-    </div>
+    <ProductProvider data={product}>
+      <Section padding="x" className="px-0"></Section>
+      <div className="grid items-start md:gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+        {/* <ProductGallery
+          media={media.nodes}
+          className="w-screen md:w-full lg:col-span-2"
+        /> */}
+        <div className="sticky md:-mb-nav md:top-nav md:-translate-y-nav md:h-screen md:pt-nav hiddenScroll md:overflow-y-scroll">
+          <section className="flex flex-col w-full max-w-xl gap-8 p-6 md:mx-auto md:max-w-sm md:px-0">
+            <div className="grid gap-2">
+              <Heading as="h1" format className="whitespace-normal">
+                {title}
+              </Heading>
+              {vendor && (
+                <Text className={"opacity-50 font-medium"}>{vendor}</Text>
+              )}
+            </div>
+            {/* <ProductForm /> */}
+            <div className="grid gap-4 py-4">
+              {descriptionHtml && (
+                <ProductDetail
+                  title="Product Details"
+                  content={descriptionHtml}
+                />
+              )}
+              {shippingPolicy?.body && (
+                <ProductDetail
+                  title="Shipping"
+                  content={getExcerpt(shippingPolicy.body)}
+                  learnMore={`/policies/${shippingPolicy.handle}`}
+                />
+              )}
+              {refundPolicy?.body && (
+                <ProductDetail
+                  title="Returns"
+                  content={getExcerpt(refundPolicy.body)}
+                  learnMore={`/policies/${refundPolicy.handle}`}
+                />
+              )}
+            </div>
+          </section>
+        </div>
+      </div>
+
+      {/* <pre>{JSON.stringify(product, null, 2)}</pre>
+      <pre>{JSON.stringify(shop, null, 2)}</pre> */}
+    </ProductProvider>
     // <ProductOptionsProvider data={product}>
     //   <Section padding="x" className="px-0">
     //     <div className="grid items-start md:gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
